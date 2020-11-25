b7ef68e0585bcd4f232a56c718b84eac392bfeec
wkdwilliams <31390496+wkdwilliams@users.noreply.github.com>
2020-11-25 01:35:09 +0000

    Change Admin password verification to Bcrypt

--- src/sjs-admin/page_password.php
+++ src/sjs-admin/page_password.php
@@ -8,12 +8,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	} elseif ($_POST['new_password'] != $_POST['verify_password']) {
 		$smarty->assign('error', 'The new password and verification password do not match.');
 	} else {
+        $_POST['new_password'] = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
 		$result = $db->query('
-			UPDATE 
-				'.DB_PREFIX.'admin 
-			SET 
-				password = md5(\'' . $db->getConnection()->real_escape_string($_POST['new_password']) . '\') 
-			WHERE 
+			UPDATE
+				'.DB_PREFIX.'admin
+			SET
+				password = "' . $_POST['new_password'] . '"
+			WHERE
 				id = 1
 		');
 
